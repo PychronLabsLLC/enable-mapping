fdfde07384f474b25f4735b9db0a8f0712f9ad8a
Jonathan Rocher <jrocher@enthought.com>
2016-01-21 17:15:27 -0600

    Flake8 on new test.

--- mapping/enable/tests/test_canvas.py
+++ mapping/enable/tests/test_canvas.py
@@ -8,10 +8,10 @@ from mapping.enable.api import MBTileManager
 
 class TestMappingCanvas(TestCase):
     def setUp(self):
-        tile_layer = MBTileManager(filename = 'map.mbtiles',
-                                   min_level=2,  max_level=4)
+        tile_layer = MBTileManager(filename='map.mbtiles',
+                                   min_level=2, max_level=4)
 
-        self.canvas = MappingCanvas(tile_cache = tile_layer)
+        self.canvas = MappingCanvas(tile_cache=tile_layer)
 
     def test__blank_tile(self):
         self.assertIsInstance(self.canvas._blank_tile, Image)
